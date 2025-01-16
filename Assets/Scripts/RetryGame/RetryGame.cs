9c0b963e60e68f6b8a5e482669a8afc575af1ea3
LizaChepurko <lizachep@gmail.com>
2025-01-16 14:17:53 +0200

    instructions update

---
+++ Assets/Scripts/RetryGame/RetryGame.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetryGame : MonoBehaviour
+{
+
+    public void PlayAgain()
+    {
+        if (SceneController.instance != null)
+        {
+            SceneController.instance.Reset();
+        }
+        if (PlayerManager.instance != null)
+        {
+            PlayerManager.instance.Reset();
+        }
+        if (Timer.Instance != null)
+        {
+            Timer.Instance.ResetTimer();
+        }
+        if (UIManager.instance != null)
+        {
+            UIManager.instance.Reset();
+        }
+        SceneManager.LoadScene("Intro"); // The main scene to start again
+    }
+}
